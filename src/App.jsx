62f979a5b85e09abb4603bf3dbafa77b8269544f
Nicolas Athayde <fpxt@hotmail.com>
2026-07-29 20:38:53 -0300

    Create dashboard page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import Dashboard from './pages/Dashboard'
+
 function App() {
   return (
-    <main>
-      <h1>DevTrack</h1>
-      <p>Organize seus estudos e projetos de desenvolvimento.</p>
-    </main>
+    <Dashboard />
   )
 }
 
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <main>
+      <h1>DevTrack Dashboard</h1>
+      <p>Acompanhe seus projetos e evolução.</p>
+    </main>
+  )
+}
+
+export default Dashboard
